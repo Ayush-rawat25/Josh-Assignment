5bc5acd6ac62a22331698d73e880cbcf08d20dff
AYUSH RAWAT <rawatayush004@gmail.com>
2026-05-13 19:39:34 +0530

    responsive-done

--- scripts/slider-script.js
+++ scripts/slider-script.js
@@ -1,10 +1,11 @@
 const swiper = new Swiper(".mySwiper", {
 
     loop: true,
-
+    loopedslide: 5,
+    loopAdditionalSlides: 2,
     centeredSlides: false,
 
-    slidesPerView: 3.4,
+    // slidesPerView: 3.4,
 
     spaceBetween: 27,
 
@@ -16,6 +17,40 @@ const swiper = new Swiper(".mySwiper", {
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
+        dynamicBullets: false
     },
+
+    breakpoints: {
+
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 15
+        },
+
+        480: {
+            slidesPerView: 1.2,
+            spaceBetween: 15
+        },
+
+        640: {
+            slidesPerView: 1.5,
+            spaceBetween: 20
+        },
+
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+
+        1024: {
+            slidesPerView: 2.5,
+            spaceBetween: 25
+        },
+
+        1280: {
+            slidesPerView: 3.4,
+            spaceBetween: 27
+        }
+    }
 
 });
